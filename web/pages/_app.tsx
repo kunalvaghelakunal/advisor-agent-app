ecb7e717dc8f4f0efea22b6c08b2620dfefa328c
kunalvaghelakunal <vaghelakunal@gmail.com>
2025-10-11 21:23:54 -0400

    Update _app.tsx

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,17 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
-// ✅ If you are using NextAuth (Google Sign-In)
-// uncomment the next 2 lines and wrap the <Component /> inside <SessionProvider>
-// import { SessionProvider } from "next-auth/react";
-
-export default function App({ Component, pageProps }: AppProps) {
-  // return (
-  //   <SessionProvider session={pageProps.session}>
-  //     <Component {...pageProps} />
-  //   </SessionProvider>
-  // );
-
-  // 👇 Use this version if you're not actively using NextAuth yet
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
