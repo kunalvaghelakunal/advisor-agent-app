b46ddfcb5ca5c6482116e1eb7abbf5f54cebc214
kunalvaghelakunal <vaghelakunal@gmail.com>
2025-10-11 21:19:02 -0400

    Update _app.tsx

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,11 +1,7 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
+// web/pages/_app.tsx
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
